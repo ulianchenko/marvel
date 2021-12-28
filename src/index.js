51297dc609ac8d708a81cfe0453f947147ced6f1
Ulianchenko <ulianchenko@gmail.com>
2021-12-28 17:24:17 +0200

    request services was added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
+// marvelService.getCharacter(1009224).then(res => console.log(res.data.results));
 
 ReactDOM.render(
   <React.StrictMode>
